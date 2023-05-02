1563a296db43a4c28be247e6503705f42d2fbf8d
avishai <avishayg1986@gmail.com>
2023-05-02 18:08:11 +0300

    17

--- 17-router-hooks-and-links/src/Header.js
+++ 17-router-hooks-and-links/src/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = () => {
   return (
     <header>
-      <div className="p-1 fs-1 bg-info text-start">
+      <div className="p-1 fs-1 bg-purple-300 text-start ">
         ColorCode Blog 👨🏻‍💻 🤔
       </div>
     </header>
--- 17-router-hooks-and-links/src/NewPost.js
+++ 17-router-hooks-and-links/src/NewPost.js
@@ -2,9 +2,23 @@ import React from 'react'
 
 const NewPost = () => {
   return (
-    <main>
-        <h1>NewPost</h1>
-    </main>
+
+          <div className="container p-2 mt-3">
+      <div className="row justify-content-center">  
+      <div className="col-10">      
+      <h2>New Post</h2>
+        <form>
+        <label htmlFor="postTitle" className='fs-5 fw-light'>Post Title</label>
+        <input type="text" class="form-control" id="postTitle" placeholder="Title"/>
+        <label htmlFor="postBody" className='fs-5 fw-light'>Post Title</label>
+        <input type="text" class="form-control" id="postBody" placeholder="Title"/>
+        </form>
+        </div>
+        </div>
+
+
+        
+    </div>
   )
 }
 
